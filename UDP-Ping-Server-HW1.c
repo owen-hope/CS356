de88c1daa80374c47e7fae0e0b9d3f719e333d7c
Owen Hope <ohopesoccer@gmail.com>
2018-01-31 12:17:14 -0500

    RTT calc isssues

--- UDP-Ping-Server-HW1.c
+++ UDP-Ping-Server-HW1.c
@@ -47,7 +47,7 @@ int main(int argc, char const *argv[]) {
       arrayConverted[i] = ntohs(arrayReceived[i]);
     }
     printf("%i\n", arrayConverted[0]);
-    usleep(usleepRandTime * 10000);
+    usleep(usleepRandTime * 100000);
     //packet fake packet loss
     if (randomNum < 4) {
       printf("This is a simulated packet loss\n");
