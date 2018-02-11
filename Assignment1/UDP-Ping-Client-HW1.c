40380885e1d72a503e52ba3558aa960395ffe661
Owen Hope <ohopesoccer@gmail.com>
2018-02-11 18:05:07 -0500

    orginization

---
+++ Assignment1/UDP-Ping-Client-HW1.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define PINGAMOUNT 10
+#define PINGREQUEST 1
+
+double RTTCalculation (time_t start, time_t end) {
+  double RTTCalc;
+
+  RTTCalc = ((double)(end - start));
+  return RTTCalc;
+}
+
+int main(int argc, char const *argv[]) {
+  //the array [0] is message type [1] is sequence number
+  int client_socket, n, packetsRecieved = 0;
+  double packetsLost = 0;
+  //Can set minRTT to 1 because if it is longer then 1 sec it times out
+  double minRTT = 1, maxRTT = 0, aveRTT = 0;
+  int percentPacketLoss;
+  int portNum;
+  int pingCount = 1;
+  int messages[2];
+  int serverMessages[2];
+  int serverMessagesConverted[2];
+  socklen_t addr_len;
+  const char *IP;
+  //clock_t time;
+  time_t start_time, end_time;
+
+  IP = argv[1];
+  portNum = atoi(argv[2]);
+
+  if (portNum != 9007) {
+    perror("incorrect port number use 9007");
+    return 1;
+  }
+
+  client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+  if (client_socket < 0) {
+    printf("cannot create socket\n");
+    return 1;
+  }
+
+  struct sockaddr_in client_address;
+  addr_len = sizeof(client_address);
+
+  //specify an address for the socket to be sent to
+  struct sockaddr_in server_address;
+
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(portNum);
+  inet_aton(IP, &server_address.sin_addr);
+
+  //Set the timeout for the socket waiting for response
+  struct timeval tv;
+  tv.tv_sec = 1;
+  tv.tv_usec = 0;
+  setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*) &tv, sizeof(tv));
+
+  printf("Pinging %s, %d:\n", IP, portNum);
+  while (pingCount <= PINGAMOUNT) {
+
+    //populate the first element in array message type & set in byte order
+    messages[0] = htons(PINGREQUEST);
+    //populates second element in array to the sequence number & set it in byte order
+    messages[1] = htons(pingCount);
+
+    //get start time to calculate RTT
+    clock_t begin = clock();
+
+    n = sendto(client_socket, messages, sizeof(messages), 0, (struct sockaddr*)
+      &server_address, sizeof(server_address));
+    if (n < 0) {
+      perror("send failed");
+      return 1;
+    }
+    //sleep(2);
+    n = recvfrom(client_socket, serverMessages, sizeof(serverMessages), 0,
+      (struct sockaddr*) &client_address, &addr_len);
+
+    //get end time to calculate RTT
+    clock_t end = clock();
+
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+    for (int i = 0; i < 2; i++) {
+      serverMessagesConverted[i] = ntohs(serverMessages[i]);
+    }
+
+    if (n <= 0) {
+      printf("Ping message number %d timed out\n", pingCount);
+      packetsLost += 1;
+    } else if (serverMessagesConverted[0] == 2) {
+      printf("ping message number %d RTT: %f secs\n", pingCount,
+        time_spent);
+      packetsRecieved += 1;
+
+      if (time_spent < minRTT) {
+        minRTT = time_spent;
+      }
+      if (time_spent > maxRTT) {
+        maxRTT = time_spent;
+      }
+
+      aveRTT += time_spent;
+    }
+
+    pingCount += 1;
+  }
+  aveRTT = aveRTT / packetsRecieved;
+  percentPacketLoss = ((packetsLost/PINGAMOUNT) * 100);
+  printf("Number of packes sent: %i Recieved: %i Loss rate: %i%%\n",
+    PINGAMOUNT, packetsRecieved, percentPacketLoss);
+
+  printf("Min RTT: %f Max RTT: %f Ave RTT: %f\n", minRTT, maxRTT, aveRTT);
+
+  close(client_socket);
+  return 0;
+}
---
+++ Assignment1/UDP-Ping-Server-HW1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#define PORTNUM 9007
+
+int main(int argc, char const *argv[]) {
+  //the array [0] is message type [1] is sequence number
+  int server_socket, client_socket, n, randomNum, usleepRandTime;
+  char msg[1024];
+  int arrayConverted[2];
+  int arrayReceived[2];
+  int arrayToSend[2];
+  socklen_t len;
+
+  //create the server sockt
+  server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+  //define the server address;
+  struct sockaddr_in server_address;
+  struct sockaddr_in client_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORTNUM);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+
+  //bind the socket to our specified IP and port
+  bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+  //while loop that continuosly runs and sends message back to client
+  while (1) {
+    //Random number generator 0-10
+    srand(time(NULL));
+    randomNum = rand() % 11;
+    usleepRandTime = rand() %30 + 1;
+
+    len = sizeof(client_address);
+    n = recvfrom(server_socket, arrayReceived, sizeof(arrayReceived), 0, (struct sockaddr*)
+    &client_address, &len);
+    //convert from byte order to int;
+    for (int i = 0; i < 2; i++) {
+      arrayConverted[i] = ntohs(arrayReceived[i]);
+    }
+    printf("%i\n", arrayConverted[0]);
+    usleep(usleepRandTime * 1000);
+    //packet fake packet loss
+    if (randomNum < 4) {
+      printf("This is a simulated packet loss\n");
+    }else if (arrayConverted[0] == 1 && randomNum >= 4) {
+      arrayConverted[0] = 2;
+      printf("%i\n", arrayConverted[0]);
+
+      //convert array back to byte order to send
+      for (int i = 0; i < 2; i++) {
+        arrayToSend[i] = htons(arrayConverted[i]);
+      }
+      //usleep(100);
+      sendto(server_socket, arrayToSend, sizeof(arrayToSend), 0,
+        (struct sockaddr*) &client_address, len);
+    }
+  }
+  close(server_socket);
+  return 0;
+}
