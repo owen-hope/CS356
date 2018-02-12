2ddb8f29009645a8637489bb7bcdeb9a334cc373
Owen Hope <ohopesoccer@gmail.com>
2018-02-11 19:22:08 -0500

    remove lines with #

--- Assignment2/DNS_Server.py
+++ Assignment2/DNS_Server.py
@@ -3,7 +3,10 @@ from socket import *
 lines = []
 with open("dns-master.txt", "r") as file:
     for line in file:
-        line = line.strip()
-        lines.append(line)
+        if line.startswith("#"):
+            continue
+        else:
+            line = line.strip()
+            lines.append(line)
 
 print(lines)
