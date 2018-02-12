c7f2e96f53ea17280b65c9d07e4e46f6202414ce
Owen Hope <ohopesoccer@gmail.com>
2018-02-11 22:36:57 -0500

    creating dict

--- Assignment2/DNS_Server.py
+++ Assignment2/DNS_Server.py
@@ -18,8 +18,12 @@ authoritative_domain = myList[0]
 TTL = myList[1]
 
 info = myList[2:]
+
+DNS_Log = {words[0]:words[1:] for words in info}
 print("\n")
 print("\n")
 print(myList)
 print("\n")
 print(info)
+print("\n")
+print(DNS_Log)
