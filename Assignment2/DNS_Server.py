3b0c5b3cbef571db7b89a7c069edb9f19f401697
owen-hope <ohopesoccer@gmail.com>
2018-02-14 07:40:22 +0000

    adsf

--- Assignment2/DNS_Server.py
+++ Assignment2/DNS_Server.py
@@ -72,8 +72,8 @@ while True:
             for i in range(firstByte[0]):
                 hostname += struct.unpack_from("!c", data, QUESTIONOFFSET)[0].decode()
                 print(hostname)
-                if i == firstByte[0] and count > 2:
-                    hostname += "."
+                if i == firstByte[0] and count < 2:
+                   # hostname += "."
                     cout += 1
                 QUESTIONOFFSET += 1
 
