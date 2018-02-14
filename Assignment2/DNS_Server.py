7c7070b5b694b30de074145137ee12c32d168076
Owen Hope <ohopesoccer@gmail.com>
2018-02-14 17:01:20 -0500

    fd

--- Assignment2/DNS_Server.py
+++ Assignment2/DNS_Server.py
@@ -163,7 +163,7 @@ while True:
     print(ipaddr)
     ipdata = ipaddr.split(".")
     print(ipdata)
-    for i in range(ipdata):
+    for i in len(ipdata):
         ipdata[i] = int(ipdata[i])
         the_message += struct.pack("!BBBB", ipdata[0], ipdata[1], ipdata[2], ipdata[3])
 
