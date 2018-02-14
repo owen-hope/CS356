ad6a7e6f548a898f23e6dd1dc57ee4312841c11f
Owen Hope <ohopesoccer@gmail.com>
2018-02-14 12:11:03 -0500

    dasf

--- Assignment2/DNS_Server.py
+++ Assignment2/DNS_Server.py
@@ -77,6 +77,8 @@ while True:
 
             hostname += "."
     hostname = hostname[:-1]
+    qtype = struct.unpack_from("!H", data, QUESTIONOFFSET+1)
+    print(qtype)
     print("out of while loop")
     print(hostname)
     for i in DNS_Log:
